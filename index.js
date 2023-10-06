c65eaec30e1184a1d20333bbc49778d635c617e6
Angelo Gentile III <angelo.john.gentile@gmail.com>
2023-10-05 22:57:14 -0400

    completed send data with fetch

--- index.js
+++ index.js
@@ -1,1 +1,59 @@
 // Add your code here
+/*
+const formValues = {
+    dogName: 'Sophia',
+    dogBreed: 'Maltese-Poodle',
+}
+
+const addNewDog = {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+    },
+    body: JSON.stringify(formValues),
+}
+
+fetch("http://localhost:3000/dogs", addNewDog)
+    .then(function (response){
+        return response.json
+    })
+    .them(function(object){
+        console.log(object)
+    })
+    .catch(function(error){
+        alert('why!?!');
+        console.log(error)
+    })
+*/
+
+let userName = 'Angelo';
+let emailAddress = 'angelo@email.com'
+
+function submitData (userName, emailAddress){
+    console.log(JSON.stringify({
+        name: userName,
+        email: emailAddress,
+    }))
+    return fetch('http://localhost:3000/users',{
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify({
+            name: userName,
+            email: emailAddress,
+        })
+    })
+    .then(response => response.json())
+    .then(info => {
+        document.body.innerHTML = info.id
+    })
+    .catch(error => {
+        document.body.innerHTML = 'Unauthorized Access';
+        console.log(error)
+    })
+}
+
+submitData(userName, emailAddress);
